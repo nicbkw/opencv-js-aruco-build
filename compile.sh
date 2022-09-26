a74b524a3f2b3fefb4953665491415b18d86bebb
Jake Howard <git@theorangeone.net>
2022-09-26 22:48:50 +0100

    Just set `$EMSCRIPTEN` variable

--- compile.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-cd opencv/
-
-python3 ./platforms/js/build_js.py build_js --cmake_option "-DOPENCV_EXTRA_MODULES_PATH=../opencv-contrib/modules" --cmake_option "$@"
