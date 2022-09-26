a631d0db5b9e15c7ba691d3f6744275c59ff0111
Jake Howard <git@theorangeone.net>
2022-09-26 22:42:01 +0100

    Pass external options as separate argument

--- compile.sh
+++ compile.sh
@@ -4,4 +4,4 @@ set -ex
 
 cd opencv/
 
-python3 ./platforms/js/build_js.py build_js --cmake_option="-DOPENCV_EXTRA_MODULES_PATH=../opencv-contrib/modules $@"
+python3 ./platforms/js/build_js.py build_js --cmake_option "-DOPENCV_EXTRA_MODULES_PATH=../opencv-contrib/modules" --cmake_option "$@"
